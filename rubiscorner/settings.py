0b94ad1aa12f6e9f93550a5189b765e4b7575780
EmmaRubiH <emmahjorting89@gmail.com>
2022-11-24 20:18:55 +0000

    installed active_link and added it to my settings

--- rubiscorner/settings.py
+++ rubiscorner/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
+    'active_link',
     'django_summernote',
     'crispy_forms',
     'bookingsystem',
